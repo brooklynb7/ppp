01dbb7eefd9b1f1c37b3497cba0d727fc2c53395
brooklynb7 <brooklynb7@gmail.com>
2018-03-16 11:35:14 +0800

    add doctor and family pagge

---
+++ client/layouts/doctor.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  v-app
+    v-toolbar(app,color="blue darken-2",dark,fixed)
+      v-toolbar-title 我是医生
+    v-content
+      v-container(fluid,fill-height)
+        v-layout(align-content-start,justify-start)
+          nuxt
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null
+  })
+}
+</script>
